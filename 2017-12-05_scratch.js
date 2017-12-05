e2763550eb12a5d1290c4a072f1c881d939b6d5b
Kevon Eaglin <kceaglin@gmail.com>
2017-12-05 13:07:34 -0500

    Complete notes for morning lesson Objects and Context

---
+++ 2017-12-05_scratch.js
@@ -0,0 +1,14 @@
+geese.forEach(function(goose) {
+	birds.filter(function(bird) {
+		return bird !== goose
+})
+})
+
+var obj = {}
+
+var wdiStudent = {
+  name: 'Some Student',
+  hasLaptop: true,
+  'transit-mode': car,
+  languages: ['javascript', 'ruby', 'python']
+}
